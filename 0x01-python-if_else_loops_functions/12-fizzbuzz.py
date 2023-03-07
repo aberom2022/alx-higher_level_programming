43f8989f56b72c75f8a2d70d4d576763e81e03c0
Roman <cheforoman@gmail.com>
2023-03-07 07:48:15 -0800

    Updated

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for x in range(1, 101):
+
+
+        """check for multiples of 3 and 5 and replace with fizzbuzz"""
+        if x % 3 == 0 and x % 5 == 0:
+            print("FizzBuzz ",end="")
+        elif x % 3 == 0:
+            print("Fizz ", end="")
+        elif x % 5 == 0:
+            print("Buzz ", end ="")
+        else:
+            print("{}".format(x),end=" ")
